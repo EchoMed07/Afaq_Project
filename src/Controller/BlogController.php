28d20b6665f1dcde62441d75850415a1f6a353c1
EchoMed07 <echarhbili07@gmail.com>
2021-12-12 19:40:53 +0100

    Initialized the project with basic Skelton architecture

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -30,7 +30,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Controller used to manage blog contents in the public part of the site.
  *
- * @Route("/blog")
+ * @Route("/Acceuil")
  *
  * @author Ryan Weaver <weaverryan@gmail.com>
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
